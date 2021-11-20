d8de70e700a5a470f72d505a85b5a611ffd9737e
user.name <example@example.com>
2021-11-21 01:05:54 +0900

    Split program that shows wrong rating delta to separate file

---
+++ src/bin/show_wrong_rating_delta.rs
@@ -0,0 +1,38 @@
+use std::{io::BufReader, iter::once, path::PathBuf};
+
+use clap::Parser;
+use fs_err::File;
+use maimai_scraping::schema::latest::PlayRecord;
+
+#[derive(Parser)]
+struct Opts {
+    input_file: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    let records: Vec<PlayRecord> =
+        serde_json::from_reader(BufReader::new(File::open(&opts.input_file)?))?;
+
+    for (old, new) in once(None).chain(records.iter().map(Some)).zip(&records) {
+        let bef = old.map_or(0, |x| x.rating_result().rating().get() as i16);
+        let aft = new.rating_result().rating().get() as i16;
+        let delta = new.rating_result().delta();
+        let bef_date = match old {
+            Some(old) => format!("{}", old.played_at().time()),
+            _ => "Initial".into(),
+        };
+        if bef + delta != aft {
+            println!(
+                "{}({}) {:+} {}({})",
+                bef,
+                bef_date,
+                delta,
+                aft,
+                new.played_at().time()
+            );
+        }
+    }
+
+    Ok(())
+}
--- src/bin/tmp_restore_first_12_ratings.rs
+++ src/bin/tmp_restore_first_12_ratings.rs
@@ -1,15 +1,12 @@
-use std::{io::BufReader, iter::once, path::PathBuf};
+use std::{io::BufReader, path::PathBuf};
 
 use clap::Parser;
 use fs_err::File;
-use maimai_scraping::schema::{
-    latest::PlayRecord as NewPlayRecord, ver_20210316_2338::PlayRecord as OldPlayRecord,
-};
+use maimai_scraping::schema::ver_20210316_2338::PlayRecord as OldPlayRecord;
 
 #[derive(Parser)]
 struct Opts {
     old_file: PathBuf,
-    new_file: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -17,8 +14,6 @@ fn main() -> anyhow::Result<()> {
     let old_records: Vec<OldPlayRecord> =
         serde_json::from_reader(BufReader::new(File::open(&opts.old_file)?))?;
     let (old_record_lost, _old_record_overlapping) = old_records.split_at(12);
-    let new_records: Vec<NewPlayRecord> =
-        serde_json::from_reader(BufReader::new(File::open(&opts.new_file)?))?;
 
     for record in old_record_lost {
         let rat = record.rating_result();
@@ -30,29 +25,6 @@ fn main() -> anyhow::Result<()> {
             rat.delta(),
             rat.rating().get() * 8 / 5,
         );
-    }
-
-    for (old, new) in once(None)
-        .chain(new_records.iter().map(Some))
-        .zip(&new_records)
-    {
-        let bef = old.map_or(0, |x| x.rating_result().rating().get() as i16);
-        let aft = new.rating_result().rating().get() as i16;
-        let delta = new.rating_result().delta();
-        let bef_date = match old {
-            Some(old) => format!("{}", old.played_at().time()),
-            _ => "Initial".into(),
-        };
-        if bef + delta != aft {
-            println!(
-                "{}({}) {:+} {}({})",
-                bef,
-                bef_date,
-                delta,
-                aft,
-                new.played_at().time()
-            );
-        }
     }
 
     Ok(())
