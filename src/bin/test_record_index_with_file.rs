043be44c7eb2b335a69420f2a82a1c51d14c50e5
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-07-16 16:11:33 +0900

    Remove almost everything

--- src/bin/test_record_index_with_file.rs
+++ src/bin/test_record_index_with_file.rs
@@ -1,30 +1,3 @@
 mod test_rating_target_with_file;
 
-use std::{
-    io::{BufReader, Read},
-    path::PathBuf,
-};
-
-use clap::Parser;
-use fs_err::File;
-use scraper::Html;
-
-#[derive(Parser)]
-struct Opts {
-    input_file: PathBuf,
-}
-
-fn main() -> anyhow::Result<()> {
-    let opts = Opts::parse();
-    let html = {
-        let mut file = BufReader::new(File::open(opts.input_file)?);
-        let mut html = String::new();
-        file.read_to_string(&mut html)?;
-        Html::parse_document(&html)
-    };
-
-    let result = maimai_scraping::maimai::play_record_parser::parse_record_index(&html)?;
-    dbg!(&result);
-
-    Ok(())
-}
+fn main() {}
--- src/maimai/schema/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod latest;
-pub mod ver_20210316_2338;
