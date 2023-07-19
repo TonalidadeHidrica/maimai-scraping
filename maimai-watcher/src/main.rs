ce10b802f34fd8c3ea5dcc6a1327919652289349
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-07-20 00:29:18 +0900

    WIP: Write a server that receives slash command

--- maimai-watcher/src/main.rs
+++ maimai-watcher/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(webhook))
+        .bind(("0.0.0.0", 15342))?
+        .run()
+        .await
+}
+
+#[get("/kiet4AeraeTaebooS9ekuequia0DuNgoodooquie7AZei4uovo")]
+async fn webhook(info: web::Query<SlashCommand>) -> impl Responder {
+    "Hello world!"
+}
+
+#[derive(Deserialize)]
+#[allow(unused)]
+struct SlashCommand {
+    token: String,
+    command: String,
+    text: String,
+    response_url: String,
+    trigger_id: String,
+    user_id: String,
+    user_name: String,
+    team_id: String,
+    api_app_id: String,
 }
