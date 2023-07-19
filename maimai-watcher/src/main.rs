bffd0b05032ca32b4403c373544d813459f73caf
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-07-20 00:41:53 +0900

    Accepts simple get request

--- maimai-watcher/src/main.rs
+++ maimai-watcher/src/main.rs
@@ -1,17 +1,38 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 use serde::Deserialize;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(webhook))
-        .bind(("0.0.0.0", 15342))?
-        .run()
-        .await
+#[derive(Clone, Deserialize)]
+struct Config {
+    route: String,
+    user_id: String,
 }
 
-#[get("/kiet4AeraeTaebooS9ekuequia0DuNgoodooquie7AZei4uovo")]
-async fn webhook(info: web::Query<SlashCommand>) -> impl Responder {
-    "Hello world!"
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config: Config = toml::from_str(&fs_err::read_to_string("maimai-watcher/config.toml")?)?;
+    let route = config.route.clone();
+    Ok(HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(State {
+                config: config.clone(),
+            }))
+            .route(&route, web::get().to(webhook))
+    })
+    .bind(("0.0.0.0", 15342))?
+    .run()
+    .await?)
+}
+
+struct State {
+    config: Config,
+}
+
+async fn webhook(state: web::Data<State>, info: web::Query<SlashCommand>) -> impl Responder {
+    if state.config.user_id == info.user_id {
+        "Hello world!"
+    } else {
+        "Go away!"
+    }
 }
 
 #[derive(Deserialize)]
