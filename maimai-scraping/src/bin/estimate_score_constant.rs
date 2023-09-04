1357adf6e23ad0bfe55e0600e6a199b8b795e333
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-09-05 01:16:30 +0900

    Update "esimate score constant" to latest format

--- maimai-scraping/src/bin/estimate_score_constant.rs
+++ maimai-scraping/src/bin/estimate_score_constant.rs
@@ -2,19 +2,18 @@ use std::path::PathBuf;
 
 use clap::Parser;
 use maimai_scraping::{
+    data_collector::load_data_from_file,
     fs_json_util::read_json,
     maimai::{
         estimate_rating::ScoreConstantsStore,
         load_score_level::{self, RemovedSong},
-        rating_target_parser::RatingTargetFile,
-        schema::latest::PlayRecord,
+        Maimai,
     },
 };
 
 #[derive(Parser)]
 struct Opts {
-    input_file: PathBuf,
-    rating_target_file: PathBuf,
+    maimai_user_data_path: PathBuf,
     level_file: PathBuf,
     removed_songs: PathBuf,
     #[clap(long)]
@@ -25,15 +24,14 @@ fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let opts = Opts::parse();
-    let records: Vec<PlayRecord> = read_json(opts.input_file)?;
-    let rating_targets: RatingTargetFile = read_json(opts.rating_target_file)?;
+    let data = load_data_from_file::<Maimai, _>(&opts.maimai_user_data_path)?;
 
     let levels = load_score_level::load(opts.level_file)?;
     let removed_songs: Vec<RemovedSong> = read_json(opts.removed_songs)?;
     let mut levels = ScoreConstantsStore::new(&levels, &removed_songs)?;
 
     levels.show_details = opts.details;
-    levels.do_everything(&records, &rating_targets)?;
+    levels.do_everything(data.records.values(), &data.rating_targets)?;
 
     Ok(())
 }
