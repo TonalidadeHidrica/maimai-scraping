2ab100f3c1b436d5dbf2a3cc44938b811265b901
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2024-02-13 23:19:28 +0900

    Show all rating chages within specific date range

--- maimai-scraping/src/bin/show_wrong_rating_delta.rs
+++ maimai-scraping/src/bin/show_wrong_rating_delta.rs
@@ -1,18 +1,23 @@
 use std::{iter::once, path::PathBuf};
 
 use clap::Parser;
-use maimai_scraping::{fs_json_util::read_json, maimai::schema::latest::PlayRecord};
+use maimai_scraping::{fs_json_util::read_json, maimai::MaimaiUserData};
 
 #[derive(Parser)]
 struct Opts {
     input_file: PathBuf,
+    #[clap(long)]
+    within_specific_date: bool,
 }
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
-    let records: Vec<PlayRecord> = read_json(opts.input_file)?;
+    let data: MaimaiUserData = read_json(opts.input_file)?;
 
-    for (old, new) in once(None).chain(records.iter().map(Some)).zip(&records) {
+    for (old, new) in once(None)
+        .chain(data.records.values().map(Some))
+        .zip(data.records.values())
+    {
         let bef = old.map_or(0, |x| x.rating_result().rating().get() as i16);
         let aft = new.rating_result().rating().get() as i16;
         let delta = new.rating_result().delta();
@@ -20,7 +25,14 @@ fn main() -> anyhow::Result<()> {
             Some(old) => format!("{}", old.played_at().time()),
             _ => "Initial".into(),
         };
-        if bef + delta != aft {
+        let within_specific_date = old.is_some_and(|old| {
+            use chrono::NaiveDate;
+            let date = old.played_at().time().get().date();
+            let start = NaiveDate::from_ymd_opt(2023, 10, 25).unwrap();
+            let end = NaiveDate::from_ymd_opt(2023, 11, 5).unwrap();
+            (start..end).contains(&date)
+        });
+        if bef + delta != aft || (opts.within_specific_date && within_specific_date) {
             println!(
                 "{}({}) {:+} {}({})",
                 bef,
