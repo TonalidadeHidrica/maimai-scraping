TonalidadeHidrica/maimai-scraping

commit f400e139428c8152574b3f8f3d99452c9b029cb9

In international version, place name is unavailable

maimai-scraping/src/maimai/parser/play_record.rs
@@ -46,7 +46,7 @@ fn parse_idx_from_playlog_main_container(playlog_top_container: ElementRef) -> a
         .map_err(|e| anyhow!("Could not parse Idx: {e:?}"))
 }
 
-pub fn parse(html: &Html, idx: Idx) -> anyhow::Result<PlayRecord> {
+pub fn parse(html: &Html, idx: Idx, place_expected: bool) -> anyhow::Result<PlayRecord> {
     let playlog_top_container = iterate_playlot_top_containers(html)
         .next()
         .ok_or_else(|| anyhow!("Playlog top container was not found."))?;
@@ -80,16 +80,18 @@ pub fn parse(html: &Html, idx: Idx) -> anyhow::Result<PlayRecord> {
         .map(parse_vs_user)
         .transpose()?;
 
-    let place_name_div = html
+    let place_name = html
         .select(selector!("#placeName > span"))
         .next()
-        .ok_or_else(|| anyhow!("Place name div not found"))?;
-    let place_name = place_name_div.text().collect::<String>().into();
+        .map(|place_name_div| PlaceName::from(place_name_div.text().collect::<String>()));
+    if place_expected && place_name.is_none() {
+        bail!("Place name expected, but not found")
+    }
 
-    let gray_block = place_name_div
+    let gray_block = playlog_top_container
         .parent()
-        .ok_or_else(|| anyhow!("No parent found for place name div"))?
-        .prev_siblings()
+        .ok_or_else(|| anyhow!("No parent found for playlog top container"))?
+        .next_siblings()
         .find_map(ElementRef::wrap)
         .ok_or_else(|| anyhow!("Gray block was not found"))?;
     let (tour_members, judge_count, rating_result, max_combo, max_sync) =

maimai-scraping/src/bin/test_with_file.rs
@@ -13,7 +13,7 @@ fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     let html = Html::parse_document(&fs_err::read_to_string(opts.input_file)?);
 
-    let result = maimai_scraping::maimai::parser::play_record::parse(&html, Idx::default())?;
+    let result = maimai_scraping::maimai::parser::play_record::parse(&html, Idx::default(), true)?;
     dbg!(&result);
     let serialized = serde_json::to_string_pretty(&result)?;
     println!("{}", &serialized);

maimai-scraping/src/bin/tmp_insert_missing_record.rs
@@ -23,6 +23,7 @@ fn main() -> anyhow::Result<()> {
     let _record = play_record::parse(
         &Html::parse_document(&fs_err::read_to_string(opts.record_html)?),
         opts.idx,
+        true,
     );
     Ok(())
 }

maimai-scraping/src/maimai/mod.rs
@@ -69,7 +69,7 @@ impl SegaTrait for Maimai {
 
     type PlayRecord = PlayRecord;
     fn parse(html: &Html, idx: Idx) -> anyhow::Result<PlayRecord> {
-        parser::play_record::parse(html, idx)
+        parser::play_record::parse(html, idx, true)
     }
 
     fn play_log_detail_not_found(location: &Url) -> bool {
@@ -100,7 +100,7 @@ impl SegaTrait for MaimaiIntl {
 
     type PlayRecord = PlayRecord;
     fn parse(html: &Html, idx: Idx) -> anyhow::Result<PlayRecord> {
-        parser::play_record::parse(html, idx)
+        parser::play_record::parse(html, idx, false)
     }
 
     fn play_log_detail_not_found(location: &Url) -> bool {

maimai-scraping/src/maimai/schema/latest.rs
@@ -51,7 +51,7 @@ pub struct PlayedAt {
     #[getset(get_copy = "pub")]
     time: PlayTime,
     #[getset(get = "pub")]
-    place: PlaceName,
+    place: Option<PlaceName>,
     #[getset(get_copy = "pub")]
     track: TrackIndex,
 }

commit 96c373d62892c93e6a66e83e431eef6a06b7dd0a

Fix bug introduced by f400e13; parse error when battle result exists

maimai-scraping/src/maimai/parser/play_record.rs
@@ -92,7 +92,8 @@ pub fn parse(html: &Html, idx: Idx, place_expected: bool) -> anyhow::Result<Play
         .parent()
         .ok_or_else(|| anyhow!("No parent found for playlog top container"))?
         .next_siblings()
-        .find_map(ElementRef::wrap)
+        .filter_map(ElementRef::wrap)
+        .find(|e| selector!(".gray_block").matches(e))
         .ok_or_else(|| anyhow!("Gray block was not found"))?;
     let (tour_members, judge_count, rating_result, max_combo, max_sync) =
         parse_center_gray_block(gray_block)?;