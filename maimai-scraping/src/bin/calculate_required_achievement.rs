d626d0dced6401cf7867a1b6887d4efc4e031eb0
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2024-05-01 13:20:59 +0900

    Show achievement requirements in bulk

--- maimai-scraping/src/bin/calculate_required_achievement.rs
+++ maimai-scraping/src/bin/calculate_required_achievement.rs
@@ -1,12 +1,19 @@
 use std::fmt::Display;
 
 use anyhow::anyhow;
+use clap::Parser;
 use inquire::{CustomType, InquireError};
 use lazy_format::lazy_format;
 use maimai_scraping::maimai::{
     rating::{rank_coef, single_song_rating, ScoreConstant},
     schema::latest::AchievementValue,
 };
+
+#[derive(Parser)]
+struct Opts {
+    #[arg(long)]
+    all: bool,
+}
 
 macro_rules! check {
     ($e: expr) => {{
@@ -18,10 +25,32 @@ macro_rules! check {
     }};
 }
 fn main() -> anyhow::Result<()> {
-    let level: u8 = CustomType::new("Internal Lv.").prompt()?;
-    let level = ScoreConstant::try_from(level).map_err(|v| anyhow!("Invalid level: {v}"))?;
+    let opts = Opts::parse();
     let rating: u16 = check!(CustomType::new("Single song rating").prompt())?;
+    if opts.all {
+        for level in ScoreConstant::candidates() {
+            let res = calculate(level, rating);
+            if !res.is_empty() {
+                print!("{level}");
+                for res in res {
+                    print!(" {}", show_segment(res));
+                }
+                println!();
+            }
+        }
+    } else {
+        let level: u8 = CustomType::new("Internal Lv.").prompt()?;
+        let level = ScoreConstant::try_from(level).map_err(|v| anyhow!("Invalid level: {v}"))?;
+        for res in calculate(level, rating) {
+            println!("{}", show_segment(res));
+        }
+    }
+    Ok(())
+}
+
+fn calculate(level: ScoreConstant, rating: u16) -> Vec<[u32; 2]> {
     let mut start = None;
+    let mut res = vec![];
     for a in 0..101_0001 {
         let ok = match AchievementValue::try_from(a) {
             Ok(a) => single_song_rating(level, a, rank_coef(a)).get() == rating,
@@ -30,12 +59,15 @@ fn main() -> anyhow::Result<()> {
         if ok {
             start.get_or_insert(a);
         } else if let Some(start) = start.take() {
-            println!("[{}, {})", show_achievement(start), show_achievement(a));
+            res.push([start, a]);
         }
     }
-    Ok(())
+    res
 }
 
 fn show_achievement(a: u32) -> impl Display {
     lazy_format!("{}.{:04}", a / 10000, a % 10000)
+}
+fn show_segment([x, y]: [u32; 2]) -> impl Display {
+    lazy_format!("[{}, {})", show_achievement(x), show_achievement(y))
 }
