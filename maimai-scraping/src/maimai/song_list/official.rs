8a0b2ae914e54d04009ae45d2aea2cdcde7022ac
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2025-03-13 10:00:56 +0900

    Add PrismPlus for official song parser

--- maimai-scraping/src/maimai/song_list/official.rs
+++ maimai-scraping/src/maimai/song_list/official.rs
@@ -233,6 +233,7 @@ impl FromStr for Version {
             "240" => Buddies,
             "245" => BuddiesPlus,
             "250" => Prism,
+            "255" => PrismPlus,
             _ => bail!("Unexpected version: {value:?}"),
         };
         let suffix = y.parse()?; // Guaranteed to be in [0, 100)
