8b2fbbdaedc9dda9a90f6f5138403dad041021d2
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2025-03-13 16:45:05 +0900

    Edit favorite song by song name

--- maimai-scraping/src/bin/edit_favorite_songs.rs
+++ maimai-scraping/src/bin/edit_favorite_songs.rs
@@ -1,12 +1,15 @@
 use std::path::PathBuf;
 
+use anyhow::bail;
 use clap::Parser;
+use fs_err::read_to_string;
+use hashbrown::{HashMap, HashSet};
 use maimai_scraping::{
     api::{SegaClient, SegaClientInitializer},
     cookie_store::UserIdentifier,
     maimai::{
         favorite_songs::{fetch_favorite_songs_form, SetFavoriteSong},
-        Maimai,
+        parser, Maimai,
     },
 };
 
@@ -14,6 +17,7 @@ use maimai_scraping::{
 struct Opts {
     credentials_path: PathBuf,
     cookie_store_path: PathBuf,
+    songs_path: PathBuf,
     #[clap(flatten)]
     user_identifier: UserIdentifier,
 }
@@ -32,9 +36,30 @@ async fn main() -> anyhow::Result<()> {
     })
     .await?;
     let page = fetch_favorite_songs_form(&mut client).await?;
+
+    let mut song_name_to_idx = HashMap::<&str, Vec<_>>::new();
+    for song in &page.songs {
+        song_name_to_idx
+            .entry(song.name.as_ref())
+            .or_default()
+            .push(&song.idx);
+    }
+
+    let mut queries = HashSet::<&parser::favorite_songs::Idx>::new();
+    for name in read_to_string(opts.songs_path)?.lines() {
+        match song_name_to_idx.get(name) {
+            None => bail!("Song not found: {name:?}"),
+            Some(idxs) => queries.extend(idxs),
+        }
+    }
+
+    if queries.len() >= 30 {
+        bail!("Too many songs! ({} songs)", queries.len());
+    }
+
     SetFavoriteSong::builder()
         .token(&page.token)
-        .music(vec![&page.songs[39].idx])
+        .music(Vec::from_iter(queries))
         .build()
         .send(&mut client)
         .await?;
