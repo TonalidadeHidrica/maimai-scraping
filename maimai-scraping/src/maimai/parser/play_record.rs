f166164e8a6c8a40bcc8cd87c820830215ec0a64
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2025-09-19 00:21:52 +0900

    Update parser to support CiRCLE

--- maimai-scraping/src/maimai/parser/play_record.rs
+++ maimai-scraping/src/maimai/parser/play_record.rs
@@ -694,6 +694,21 @@ fn parse_achievement_rank(achievement_rank: ElementRef) -> anyhow::Result<Achiev
         "https://maimaidx.jp/maimai-mobile/img/playlog/b.png?ver=1.59" => B,
         "https://maimaidx.jp/maimai-mobile/img/playlog/c.png?ver=1.59" => C,
         "https://maimaidx.jp/maimai-mobile/img/playlog/d.png?ver=1.59" => D,
+        // Ver 1.60
+        "https://maimaidx.jp/maimai-mobile/img/playlog/sssplus.png?ver=1.60" => SSSPlus,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/sss.png?ver=1.60" => SSS,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/ssplus.png?ver=1.60" => SSPlus,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/ss.png?ver=1.60" => SS,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/splus.png?ver=1.60" => SPlus,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/s.png?ver=1.60" => S,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/aaa.png?ver=1.60" => AAA,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/aa.png?ver=1.60" => AA,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/a.png?ver=1.60" => A,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/bbb.png?ver=1.60" => BBB,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/bb.png?ver=1.60" => BB,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/b.png?ver=1.60" => B,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/c.png?ver=1.60" => C,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/d.png?ver=1.60" => D,
         // International
         // Ver 1.35
         "https://maimaidx-eng.com/maimai-mobile/img/playlog/sssplus.png?ver=1.35" => SSSPlus,
@@ -888,6 +903,12 @@ fn parse_full_combo_img(full_combo_img: ElementRef) -> anyhow::Result<FullComboK
         "https://maimaidx.jp/maimai-mobile/img/playlog/fcplus.png?ver=1.59" => FullComboPlus,
         "https://maimaidx.jp/maimai-mobile/img/playlog/ap.png?ver=1.59" => AllPerfect,
         "https://maimaidx.jp/maimai-mobile/img/playlog/applus.png?ver=1.59" => AllPerfectPlus,
+        // Ver 1.60
+        "https://maimaidx.jp/maimai-mobile/img/playlog/fc_dummy.png?ver=1.60" => Nothing,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/fc.png?ver=1.60" => FullCombo,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/fcplus.png?ver=1.60" => FullComboPlus,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/ap.png?ver=1.60" => AllPerfect,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/applus.png?ver=1.60" => AllPerfectPlus,
         // International
         // Ver 1.35
         "https://maimaidx-eng.com/maimai-mobile/img/playlog/fc_dummy.png?ver=1.35" => Nothing,
@@ -978,6 +999,13 @@ fn parse_full_sync_img(full_sync_img: ElementRef) -> anyhow::Result<FullSyncKind
         "https://maimaidx.jp/maimai-mobile/img/playlog/fsplus.png?ver=1.59" => FullSyncPlus,
         "https://maimaidx.jp/maimai-mobile/img/playlog/fsd.png?ver=1.59" => FullSyncDx,
         "https://maimaidx.jp/maimai-mobile/img/playlog/fsdplus.png?ver=1.59" => FullSyncDxPlus,
+        // Ver 1.60
+        "https://maimaidx.jp/maimai-mobile/img/playlog/sync_dummy.png?ver=1.60" => Nothing,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/sync.png?ver=1.60" => SyncPlay,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/fs.png?ver=1.60" => FullSync,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/fsplus.png?ver=1.60" => FullSyncPlus,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/fsd.png?ver=1.60" => FullSyncDx,
+        "https://maimaidx.jp/maimai-mobile/img/playlog/fsdplus.png?ver=1.60" => FullSyncDxPlus,
         // International
         // Ver 1.35
         "https://maimaidx-eng.com/maimai-mobile/img/playlog/sync_dummy.png?ver=1.35" => Nothing,
@@ -1454,6 +1482,18 @@ fn parse_rating_color(img: ElementRef) -> anyhow::Result<RatingBorderColor> {
         "https://maimaidx.jp/maimai-mobile/img/rating_base_gold.png?ver=1.59" => Gold,
         "https://maimaidx.jp/maimai-mobile/img/rating_base_platinum.png?ver=1.59" => Platinum,
         "https://maimaidx.jp/maimai-mobile/img/rating_base_rainbow.png?ver=1.59" => Rainbow,
+        // Ver 1.60
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_normal.png?ver=1.60" => Normal,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_blue.png?ver=1.60" => Blue,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_green.png?ver=1.60" => Green,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_orange.png?ver=1.60" => Orange,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_red.png?ver=1.60" => Red,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_purple.png?ver=1.60" => Purple,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_bronze.png?ver=1.60" => Bronze,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_silver.png?ver=1.60" => Silver,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_gold.png?ver=1.60" => Gold,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_platinum.png?ver=1.60" => Platinum,
+        "https://maimaidx.jp/maimai-mobile/img/rating_base_rainbow.png?ver=1.60" => Rainbow,
         // International
         // Ver 1.35
         "https://maimaidx-eng.com/maimai-mobile/img/rating_base_normal.png?ver=1.35" => Normal,
--- maimai-scraping/src/maimai/parser/song_score.rs
+++ maimai-scraping/src/maimai/parser/song_score.rs
@@ -216,6 +216,21 @@ fn parse_achievement_rank(achievement_rank: ElementRef) -> anyhow::Result<Achiev
         "https://maimaidx.jp/maimai-mobile/img/music_icon_b.png?ver=1.59" => B,
         "https://maimaidx.jp/maimai-mobile/img/music_icon_c.png?ver=1.59" => C,
         "https://maimaidx.jp/maimai-mobile/img/music_icon_d.png?ver=1.59" => D,
+        // Ver 1.60
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_sssp.png?ver=1.60" => SSSPlus,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_sss.png?ver=1.60" => SSS,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_ssp.png?ver=1.60" => SSPlus,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_ss.png?ver=1.60" => SS,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_sp.png?ver=1.60" => SPlus,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_s.png?ver=1.60" => S,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_aaa.png?ver=1.60" => AAA,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_aa.png?ver=1.60" => AA,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_a.png?ver=1.60" => A,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_bbb.png?ver=1.60" => BBB,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_bb.png?ver=1.60" => BB,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_b.png?ver=1.60" => B,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_c.png?ver=1.60" => C,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_d.png?ver=1.60" => D,
         src => bail!("Unknown src for achievement rank: {src:?}"),
     };
     Ok(res)
@@ -252,6 +267,12 @@ fn parse_full_combo_img(full_combo_img: ElementRef) -> anyhow::Result<FullComboK
         "https://maimaidx.jp/maimai-mobile/img/music_icon_fcp.png?ver=1.59" => FullComboPlus,
         "https://maimaidx.jp/maimai-mobile/img/music_icon_ap.png?ver=1.59" => AllPerfect,
         "https://maimaidx.jp/maimai-mobile/img/music_icon_app.png?ver=1.59" => AllPerfectPlus,
+        // Ver 1.60
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_back.png?ver=1.60" => Nothing,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_fc.png?ver=1.60" => FullCombo,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_fcp.png?ver=1.60" => FullComboPlus,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_ap.png?ver=1.60" => AllPerfect,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_app.png?ver=1.60" => AllPerfectPlus,
         src => bail!("Unknown src for full combo img: {src:?}"),
     };
     Ok(res)
@@ -291,6 +312,13 @@ fn parse_full_sync_img(full_sync_img: ElementRef) -> anyhow::Result<FullSyncKind
         "https://maimaidx.jp/maimai-mobile/img/music_icon_fsp.png?ver=1.59" => FullSyncPlus,
         "https://maimaidx.jp/maimai-mobile/img/music_icon_fdx.png?ver=1.59" => FullSyncDx,
         "https://maimaidx.jp/maimai-mobile/img/music_icon_fdxp.png?ver=1.59" => FullSyncDxPlus,
+        // Ver 1.60
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_back.png?ver=1.60" => Nothing,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_sync.png?ver=1.60" => SyncPlay,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_fs.png?ver=1.60" => FullSync,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_fsp.png?ver=1.60" => FullSyncPlus,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_fdx.png?ver=1.60" => FullSyncDx,
+        "https://maimaidx.jp/maimai-mobile/img/music_icon_fdxp.png?ver=1.60" => FullSyncDxPlus,
         src => bail!("Unknown src for full sync img: {src:?}"),
     };
     Ok(res)
